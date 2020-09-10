225d99db94bceb37d16ebef8a08252476879d96b
Caleb335 <belac335@gmail.com>
2020-09-10 14:41:07 +0100

    fix prettier script

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-    return (
-        <div className="app__base">
-            <h1>Welcome to rrp</h1>
-            <p>Edit App.js file in the components folder to get started.</p>
-        </div>
-    )
-}
+  return (
+    <div className="app__base">
+      <h1>Welcome to rrp</h1>
+      <p>Edit App.js file in the components folder to get started.</p>
+    </div>
+  );
+};
 
-export default App
+export default App;
