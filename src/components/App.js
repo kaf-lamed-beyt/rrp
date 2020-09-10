34512ae80257a693bb4ee9365a801cad16472671
Caleb335 <belac335@gmail.com>
2020-09-10 08:06:39 +0100

    add entry point to the app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to rrp</h1>
+            <p>Edit App.js file in the components folder to get started.</p>
+        </div>
+    )
+}
+
+export default App
