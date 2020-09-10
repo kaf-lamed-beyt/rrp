d02c0b114c09436f7b9a06616cc972d0510ee486
Caleb335 <belac335@gmail.com>
2020-09-10 08:23:43 +0100

    linked styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>
+        <div className="app__base">
             <h1>Welcome to rrp</h1>
             <p>Edit App.js file in the components folder to get started.</p>
         </div>
